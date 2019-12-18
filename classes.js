0daa4eb02d65ba1ee3b4249aeae55bf4d7f9f184
Dimitri Tabatadze <35934791+ddddtttt6666@users.noreply.github.com>
2019-12-18 20:56:30 +0400

    final commit

--- classes.js
+++ classes.js
@@ -1,5 +1,6 @@
 class Game_world{
 	constructor(){
+		this.tot_coins = 0
 		// initialize the map as an empty array
 		this.map = [];
 		// fill it up with cells
@@ -7,22 +8,57 @@ class Game_world{
 			this.map[i] = []
 			for(let j = 0; j < nCells; j++){
 				this.map[i][j] = new Cell(j, i)
+				if(this.map[i][j].type == 3){
+					this.tot_coins += 1;
+				}
 			}
 		}
 
+		if(this.tot_coins == 0){
+			restart()
+		}
+
+		this.map[1][1].type = 2;
+
 		// initialize the player
-		this.player = new Player(this.update_map);
+		this.player = new Player(this.update_map, this);
 		// pass a callback function for updating maps to the player
 		this.player.update_map(this.map);
+		this.win = {'state': false,
+					'counter': 60};
 
+		this.time_limit = {'counter': 240,
+						   'counter_max': 240,
+						   'state': true};
 	}
-
 	// a callback function for player to call when it needs to change the map
 	update_map(map){
+
 		this.map = map;
 	}
 
 	update(){
+		if(this.win.counter <= 0){
+			restart();
+		}
+
+		if(this.win.state){
+			this.win.counter -= 1;
+		}
+
+		if(this.tot_coins <= 0 && !this.win.state){
+			this.win.state = true
+			this.player.reset()
+		}
+
+		if(this.time_limit.counter > 0 && !this.win.state){
+			this.time_limit.counter -= 1;
+		}else{
+			this.time_limit.state = false;
+			this.player.reset();
+			this.win.state = true
+		}
+
 		// update the player
 		this.player.update();
 	}
@@ -35,28 +71,45 @@ class Game_world{
 			}
 		}
 
+		if(this.win.state){
+			background(51, map(this.win.counter, 90, 0, 0, 255));
+		}
+
 		// show the player
 		this.player.show();
+
+		let time = this.time_limit.counter/this.time_limit.counter_max;
+		stroke((1-time)*255, (time)*255, 0);
+		strokeWeight(6);
+		line(3, 3, time*width, 3)
 	}
 }
 
 class Player{
-	constructor(map_updater, x=1, y=1){
+	constructor(map_updater, parent, x=1, y=1){
 		this.position = createVector(x || 1, y || 1);
 		this.p_position = this.position.copy();
 		this.target_position = createVector(x, y);
 		this.p_target_position = createVector(x, y);
 		this.map;
 		this.map_updater = map_updater;
+		this.parent = parent;
 		this.number_of_coins = 0;
+		this._win = {'state': false, 'move_to_start_step': 1}
+	}
+
+	reset(){
+		this._win.state = true;
+		this._win.move_to_start_step = createVector(1, 1).sub(this.position).div(this.parent.win.counter);
 	}
 
 	update_map(map){
+
 		this.map = map;
 	}
 
 	move(move_vector){
-		if(this.position.equals(this.target_position)){
+		if(this.position.equals(this.target_position) && !this._win.state){
 			let no_obstacle = true;
 			while(no_obstacle){
 				// calculate future position
@@ -77,10 +130,13 @@ class Player{
 	}
 
 	update(){
+		if(!this.parent.win.state){
+			this._win.state = false
+		}
 		// save current position as previous postion for next update() step
 		this.p_position = this.position.copy();
 
-		if(!this.position.equals(this.target_position)){
+		if(!this.position.equals(this.target_position) && !this._win.state){
 			// get the step vector
 			let step = this.target_position.copy().sub(this.position);
 			// size the step vector properly
@@ -94,38 +150,46 @@ class Player{
 			if(y_check || x_check){
 				// check for every possible cell type other then the type 1 which is a solid
 				// wall and we can't ever be standing there.
-				if(this.map[floor(this.p_position.y)][floor(this.p_position.x)].type == 0){
+				if(this.map[floor(this.position.y)][floor(this.position.x)].type == 0){
 					// if the cell the player is standing on is an empty space, turn it into 'half-wall'
-					this.map[floor(this.p_position.y)][floor(this.p_position.x)].type = 2;
-				}else if(this.map[floor(this.p_position.y)][floor(this.p_position.x)].type == 2){
+					this.map[floor(this.position.y)][floor(this.position.x)].type = 2;
+				}else if(this.map[floor(this.position.y)][floor(this.position.x)].type == 2){
 					// if the cell the player is standing on is a 'half-wall' turn it into a 'wall'
-					this.map[floor(this.p_position.y)][floor(this.p_position.x)].type = 1;
-				}else if(this.map[floor(this.p_position.y)][floor(this.p_position.x)].type == 3){
-					// if the cell tha player is standing on is a 'coin' pick it up and 
-					// turn it into a 'half-wall'
+					this.map[floor(this.position.y)][floor(this.position.x)].type = 1;
+				}
+				if(this.map[floor(this.position.y)][floor(this.position.x)].type == 3){
 					this.number_of_coins += 1;
-					this.map[floor(this.p_position.y)][floor(this.p_position.x)].type = 2;
+					this.parent.tot_coins -= 1;
+					this.map[floor(this.position.y)][floor(this.position.x)].type = 2;
 				}
 				// update the map
 				this.map_updater(this.map);
 			}
+
+		}
+
+		if(this._win.state){
+			this.position.add(this._win.move_to_start_step);
 		}
 	}
 
 	show(){
 		// draw the player
+		// stroke(0, 240);
+		// strokeWeight(4);
 		noStroke();
 		fill(3, 252, 152);
 		rect(this.position.x*CELL_SIZE, this.position.y*CELL_SIZE, CELL_SIZE, CELL_SIZE);
-		if(!this.target_position.equals(this.p_target_position)){
-			fill(3, 252, 152, 160);
-			rect(this.p_position.x*CELL_SIZE, this.p_position.y*CELL_SIZE, CELL_SIZE, CELL_SIZE);
-		}
-
 		// draw the amount of coins the player is holding
 		fill(210, 90, 255);
 		textSize(32);
 		text(this.number_of_coins, 10, 30);
+
+		if(!this.target_position.equals(this.p_target_position) && !this._win.state){
+			fill(3, 252, 152, 210);
+			rect(this.p_position.x*CELL_SIZE, this.p_position.y*CELL_SIZE, CELL_SIZE, CELL_SIZE);
+		}
+
 	}
 }
 
@@ -134,25 +198,32 @@ class Cell{
 		this.position = createVector(x, y);
 		this.types = ['empty-space', 'wall', 'half-wall', 'coin']
 		this.type = (x == 0 || y == 0 || x == nCells-1 || y == nCells-1 || random(1) < 0.1)? 1 : 0;
-		this.type = (x != 0 && y != 0 && x != nCells-1 && y != nCells-1 && random(1) < 0.1)? 3 : this.type;
+		if(!(x == 1 && y == 1)){
+			this.type = (x != 0 && y != 0 && x != nCells-1 && y != nCells-1 && random(1) < 0.1)? 3 : this.type;
+		}
 		this.solid = (this.type == 0)
 	}
 
 	show(){
+		noStroke();
+		if(this.type == 0 /* enpty-space */){
+			stroke(0);
+			strokeWeight(4);
+			fill(220);
+		}
 		if(this.type == 1 /* wall */){
-			noStroke();
 			fill(51);
-			rect(this.position.x*CELL_SIZE, this.position.y*CELL_SIZE, CELL_SIZE, CELL_SIZE)
 		}
 		if(this.type == 2 /* half-wall */){
-			noStroke();
+			stroke(0);
+			strokeWeight(4);
 			fill(110);
-			rect(this.position.x*CELL_SIZE, this.position.y*CELL_SIZE, CELL_SIZE, CELL_SIZE)
 		}
 		if(this.type == 3 /* coin */){
-			noStroke();
+			stroke(0);
+			strokeWeight(4);
 			fill(255, 255, 90);
-			ellipse(this.position.x*CELL_SIZE+CELL_SIZE/2.0, this.position.y*CELL_SIZE+CELL_SIZE/2, CELL_SIZE, CELL_SIZE)
 		}
+		rect(this.position.x*CELL_SIZE, this.position.y*CELL_SIZE, CELL_SIZE, CELL_SIZE);
 	}
 }
--- sketch.js
+++ sketch.js
@@ -7,20 +7,28 @@ const WORLD_SIZE = 480;
 const nCells = 8;
 const CELL_SIZE = WORLD_SIZE/nCells;
 
+
+
 let Game;
+
+let seed = 55;
+let good_seeds = {'seeds':[]};
 
 function setup(){
 	createCanvas(480, 480);
+	randomSeed(seed);
 	move.up = createVector(0, -1);
 	move.down = createVector(0, 1);
 	move.right = createVector(1, 0);
 	move.left = createVector(-1, 0);
+	textFont('fantasy');
+	textSize(60);
 	Game = new Game_world();
 }
 
 function draw(){
 	// main
-	background(220);
+	background(0, 255, 0);
 	Game.show();
 	Game.update();
 
@@ -39,4 +47,21 @@ function draw(){
 			Game.player.move(move.down);
 		}
 	}
+}
+
+
+function restart(){
+	seed += 1;
+	randomSeed(seed);
+	Game = new Game_world();
+	Game.player._win.state = false;
+	console.log(seed);
+}
+
+function save_seed(){
+	good_seeds.seeds.push(seed);
+}
+
+function show_seeds(){
+	console.log(good_seeds.seeds);
 }
